d387cadd5f70a6ca31fb12d815d3d75b63722de3
Bastini288 <33761095+Bastini288@users.noreply.github.com>
2021-04-07 17:23:35 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,6 @@ import java.util.Date;
 public class Main {
 	public static void main(String[] args) {
 		BlockChaine chaine = new BlockChaine();
-		chaine.boucleChaine(20);
+		chaine.boucleChaine(20); // chaine de 20 block
 		}
 }
