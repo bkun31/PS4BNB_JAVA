9c54fca432e1f18c9d8973168188dc634e4b0d8f
Bastini288 <33761095+Bastini288@users.noreply.github.com>
2021-04-07 17:06:42 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
-dazdzadad
+package Projet;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Main {
+	public static void main(String[] args) {
+		BlockChaine chaine = new BlockChaine();
+		chaine.boucleChaine(20);
+		}
+}
