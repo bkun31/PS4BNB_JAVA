038e623ccf0066399a5a0a9dab5301968f7858c3
Bastini288 <33761095+Bastini288@users.noreply.github.com>
2021-04-15 23:55:48 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ package Projet;
 public class Main {
 	public static void main(String[] args) {
 		BlockChaine chaine = new BlockChaine(3,10,10,50);
-		chaine.boucleChaine(50); // chaine de 20 block
+		chaine.boucleChaine(50); 
 		
 		
 		}
