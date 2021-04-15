a37d87b55a5ccd4ccdec0f273bc3fb45e64fa80a
Bastini288 <33761095+Bastini288@users.noreply.github.com>
2021-04-15 23:52:58 +0200

    Update Block.java

--- Block.java
+++ Block.java
@@ -3,6 +3,7 @@ package Projet;
 import java.util.Random;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.Arrays;
 
 public class Block {
 	private String hashPrecedent;
@@ -10,8 +11,10 @@ public class Block {
 	private int nbTransaction;
 	private int Nonce;
 	private String merkel;
-	private String HashBlock;
+	private String hashBlock;
 	private Date date;
+	private User user;
+	private int numBlock;
 	
 	/**
 	 * Contructeur avec liste de transaction
@@ -19,14 +22,16 @@ public class Block {
 	 * @param String[] listeTransaction
 	 * @param nbTransaction : nombre de transaction
 	 */
-	public Block(String hashPrecedent, String[] listeTransaction, int nbTransaction) {
+	public Block(String hashPrecedent, String[] listeTransaction, int nbTransaction, int numBlock, User user) {
 		this.hashPrecedent = hashPrecedent;
 		this.listeTransaction = listeTransaction;
 		this.nbTransaction = nbTransaction;
+		this.numBlock = numBlock;
+		this.user = user;
 		Nonce = 0;
 		date = new Date();
 		// faire le merkel des transaction
-		merkel = "54247930ee9b51c10e438ff3a2ad25c7eca5b890213b4e77d12012cc307017c0";
+		merkel = merkel();
 	}
 	
 	/**
@@ -45,7 +50,7 @@ public class Block {
 			listeTransaction[i] = generateTransaction(); // Met nbTransaction dans le tableau
 		}
 		// faire le merkel des transaction
-		merkel = "846047930ee9b51c10e438ff3a2ad25c7eca5b890213b4e77d12012cc307017c0";
+		merkel = merkel();
 		
 	}
 
@@ -65,33 +70,84 @@ public class Block {
 		}
 		return (name1 + " envoie " + Bnb +" Bnb à " + name2);
 	}
-
+	
+	
+	/**
+	 * Hash le block en incrementant le nonce jusqu'a que le block commence par 3x "000"
+	 * @param BLock a hash
+	 * @return String Hash avec  "0" x difficulte devant
+	 */
+	public String hashBlock(int difficulte) {
+		String dif = "";
+		for (int i=0; i<difficulte; i++) {
+			dif = dif +"0";
+		}
+		hashBlock = HashUtil.applySha256(hashPrecedent + merkel + dateToString(date) + Nonce); //Concatenation des info + hash
+		while (!hashBlock.substring(0, difficulte).equals(dif)) { //Verifie que les 4 premier caracter du hash soit "0" x difficulte
+			Nonce++;
+			hashBlock = HashUtil.applySha256(hashPrecedent + merkel + dateToString(date) + Nonce + numBlock + stringListeTx() + user.getNom());
+		}
+		return hashBlock;
+	}
+	
+	public String merkel() {
+		return merkelHash(listeTransaction, nbTransaction);
+	}
+	
+	public String merkelHash(String tabTx[], int nbTx) {
+		int i=0;
+		int nbTxHash=0;
+		String tab[] = Arrays.copyOf(tabTx, nbTx); //crée une copie de tabTx[] de longeur nbTx
+		String [] tabHash = tab;
+		if (nbTx == 1) {
+			return tab[0];
+		}
+		while (i<nbTx-1) {
+			tabHash[nbTxHash]= HashUtil.applySha256(tab[i] + tab[i+1]);
+			nbTxHash++;
+			i+=2;
+		}
+		if (nbTx % 2 != 0) {
+			tabHash[nbTxHash]= HashUtil.applySha256(tab[i] + tab[i]);
+			nbTxHash++;
+		}
+		return merkelHash(tabHash, nbTxHash);
+	}
+	
+	public String stringListeTx() {
+		String liste = "";
+		for (int i=0; i<nbTransaction;i++) {
+			liste = liste + listeTransaction[i] + "\n";
+		}
+		return liste;
+	}
+	
+	public void printBlock() {
+		System.out.println("Block numero (Index) : " + numBlock);
+		System.out.println("Previous hash : " + hashPrecedent);
+		System.out.println("Current hash : " + hashBlock);
+		System.out.println("Timestamp : " + dateToString(date));
+		System.out.println("Nb transaction : " + nbTransaction);
+		System.out.println("Liste transaction {");
+		System.out.println(stringListeTx());
+		System.out.println("}");
+		System.out.println("Merkel tree root : " + merkel);
+		System.out.println("Miner : " + user.getNom());
+		System.out.println("Nonce : " + Nonce);
+	}
+	
 	/**
 	 * @return the nonce
 	 */
 	public int getNonce() {
 		return Nonce;
 	}
-
-	/**
-	 * @param nonce the nonce to set
-	 */
-	public void setNonce(int nonce) {
-		Nonce = nonce;
-	}
-
+	
 	/**
 	 * @return the hashBlock
 	 */
 	public String getHashBlock() {
-		return HashBlock;
-	}
-
-	/**
-	 * @param hashBlock the hashBlock to set
-	 */
-	public void setHashBlock(String hashBlock) {
-		HashBlock = hashBlock;
+		return hashBlock;
 	}
 
 	/**
@@ -118,7 +174,14 @@ public class Block {
 	/**
 	 * @return the date
 	 */
-	public String getDate() {
+	public Date getDate() {
+	    return date;
+	  }
+	
+	/**
+	 * @return the date
+	 */
+	public static String dateToString(Date date) {
 		DateFormat shortDateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT,DateFormat.MEDIUM);
 	    return shortDateFormat.format(date);
 	  }
@@ -129,6 +192,16 @@ public class Block {
 	public String getMerkel() {
 		return merkel;
 	}
+
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+	
+	
+	
 	
 	
 	
